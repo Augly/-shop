a56b9d4fcb5cef97a248f59cfd47298788f2d36b
zero <1137774080@qq.com>
2019-05-17 01:24:31 +0800

    添加banner,Evalist,Hava_a_look,Look_around,start组件编写

---
+++ components/Banner/Banner.js
@@ -0,0 +1,33 @@
+// components/Banner/Banner.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    indicatorColor: 'rgba(255, 255, 255, 1)',
+    indicatorActiveColor: 'rgba(69, 208, 174, 1)',
+    interval: 1000,
+    duration: 1000
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ components/Evalist/Evalist.js
@@ -0,0 +1,26 @@
+// components/Evalist/Evalist.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    showAll:{
+      type:Boolean,
+      value:false
+    }
+  },
+  
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    read:true                                                              
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
